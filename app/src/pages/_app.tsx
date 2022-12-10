065df802a772714827c49d10daaa819ef19765ed
Pythagoras-theorem <takusan315@gmail.com>
2022-12-11 03:30:22 +0900

    [add] guidelineの完成．

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import type { AppProps } from "next/app";
 import "../../styles/globals.scss";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>表情補完自然度測定</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
