db03912396d9136a9cd4c4b9337b77f4196940fd
unknown <takusan315@gmail.com>
2022-12-29 17:48:28 +0900

    [add] analyze 80%

--- analyze.py
+++ analyze.py
@@ -5,8 +5,7 @@ import math
 import statistics as stcs
 import numpy as np
 import scipy as sp
-import scipy.stats
-import pandas as pd
+import scipy.stats as stats
 
 
 def morph_graph_style():
@@ -141,43 +140,122 @@ def diffByAspect(data, types):  # semanticstyleganと他の実装の比較
     for kt, vt in expectation_sm.items() :
         for kf, vf in vt.items() :
             values_mean[kt][kf] = stcs.mean(vf)
+            values_median[kt][kf] = stcs.median(vf)
 
-    for s in ['liner', 'stylegan']:
-        diff = {}
-        for k in values_mean[next(iter(values_mean))]:
-            diff[k] = values_mean['semanticstylegan'][k] - values_mean[s][k]
-        diff = sorted(diff.items(), key=lambda x: x[1])
+    lst = {'mean' : values_mean, 'median' :values_median}
+    for i in lst.keys() :
+        for s in ['liner', 'stylegan']:
+            print(f'- {i}')
+            diff = {}
+            for k in lst[i][next(iter(lst[i]))]:
+                diff[k] = lst[i]['semanticstylegan'][k] - lst[i][s][k]
+            diff = sorted(diff.items(), key=lambda x: x[1])
 
-        cnt = 0
-        print(f'-- diff {s} --')
-        for k, v in diff :
-            if v < 0 :
-                print(f'*{k}', v, sep=' : ')
-            else :
-                print(k, v, sep=' : ')
-                cnt += 1
-        print(f'改善された表情数 : {cnt}/{len(diff)} {math.floor(cnt/len(diff) * 100)}%')
+            cnt = 0
+            print(f'    - diff {s}')
+            for k, v in diff :
+                if v < 0 :
+                    print(f'        *{k}', v, sep=' : ')
+                else :
+                    print(f'        {k}', v, sep=' : ')
+                    cnt += 1
+            print(f'        改善された表情数 : {cnt}/{len(diff)} {math.floor(cnt/len(diff) * 100)}%')
 
 
-def diffReverseAspect() :
-    print('a')
+def diffReverseAspect(data, types) :
+    expectation = totallng(data, types)
+    expectation2 = {t : {f :[] for f in types['morphing']} for t in types['technique'] }
+    types_st_morphing = []
+    for vs in expectation.values() :
+        for kt, vt in vs.items() :
+            for kf, vf in vt.items() :
+                expectation2[kt][kf].extend(vf)
+                for sm in ['AN-', 'FE-', 'SA-']:  # 顔始点はAN, FE, SA
+                    if sm in kf:
+                        types_st_morphing.append(kf)
+                    break
 
-def kai(data) :
+    values_mean = {t : {f : 0 for f in types['morphing']} for t in types['technique']}
+    values_median = {t : {f : 0 for f in types['morphing']} for t in types['technique']}
+
+    for kt, vt in expectation2.items() :
+        for kf, vf in vt.items() :
+            values_mean[kt][kf] = stcs.mean(vf)
+            values_median[kt][kf] = stcs.median(vf)
     
-    x2, p, dof, expected = sp.stats.chi2_contingency(crossed)
+    values_pair = {t : [{f : 0 for f in types_st_morphing}] * 2 for t in types['technique']}
+    for kt, vt in values_pair.items() :
+        for i, vi in enumerate(vt) :
+            for kf in vi.keys() :
+                if i == 0 :
+                    values_pair[kt][i][kf]  = values_mean[kt][kf]
+                else :
+                    a, b = kf.split('-')
+                    rkey = f'{b}-{a}'
+                    values_pair[kt][i][kf]  = values_mean[kt][rkey]
+    totoal_width = 0.8
 
-    print("カイ二乗値は %(x2)s" %locals() )
-    print("確率は %(p)s" %locals() )
-    print("自由度は %(dof)s" %locals() )
-    print( expected )
+    morph_graph_style()
+    plt.title('reverse morphing', fontsize=20)
+    for kt, vt in values_pair.items() :
+        for i, ex in enumerate(vt):
+            pos = np.arange(len(ex[1])) - totoal_width * \
+                (1 - (2*i+1)/len(values_pair.values()))/2
+            plt.bar(pos, ex[1].values(), width=totoal_width /
+                    len(values_pair.values()), label=ex[0])
+    plt.legend()
+    plt.xticks(np.arange(len(values_pair[next(
+        iter(values_pair))])), values_mean[next(iter(values_pair))].keys())
+    plt.savefig(f'pdf/reverse-morphing.pdf')
 
-    if p < 0.05:
-        print("有意な差があります")
-    else:
-        print("有意な差がありません")
 
-def commentPickup() :
-    print('a')
+    
+
+def tatisticalTest(data, types) :
+    def p_show(p) :
+        if p < 0.05 :
+            print('     - significance level : モーフィング間は有意である．')
+        else :
+            print('     - significance level : モーフィング間は有意があるか分からない．')
+
+    expectation = totallng(data, types)
+    all_ans = {t : [] for t in types['technique']}
+    for vs in expectation.values() :
+        for kt, vt in vs.items() :
+            for vf in vt.values() :
+                all_ans[kt].extend(vf)
+    
+    result = stats.mstats.kruskal(all_ans['liner'] , all_ans['stylegan'], all_ans['semanticstylegan'])
+    print(f'- クラスカル = ウォリス検定')
+    print(f'    - 統計量H : {result.statistic}, p値 : {result.pvalue}')
+    p_show(result.pvalue)
+
+    for s in ['stylegan', 'liner']: 
+        result=stats.mannwhitneyu(all_ans['semanticstylegan'], all_ans[s]) # 片側検定でも良い．
+        print(f'- マンホイットニーのU検定 semanticstylegan-{s}')
+        print(f'    - 統計量U : {result.statistic}, p値 : {result.pvalue}')
+        p_show(result.pvalue)
+
+
+
+def commentPickup(data, types) :
+    comments = {t : {f : [] for f in types['morphing']} for t in types['technique'] }
+    for k, v in data.items():
+        for e in v['results']:
+            if e['comment'] != "" :
+                comments[e['type']][e['name']].append((v['sex'], k, e['comment']))
+    
+    
+    for kt, vt in comments.items() :
+        cnt = 0
+        print(f'- {kt}')
+        for kf, vf in vt.items() :
+            if len(vf) != 0 :
+                print(f'    - {kf}')
+                for v in vf :
+                    print(f'         {v[0]}, {v[1]} : {v[2]}')
+                    cnt += 1
+        print(f'        コメント数 : {cnt}')
 
 
 base_path = 'results/*.json'
@@ -197,4 +275,4 @@ for i, p in enumerate(paths):
             type_technique.add(d['type'])
             type_morphing.add(d['name'])
 types = {'sex' : list(type_sex), 'technique' : list(type_technique), 'morphing' : list(type_morphing)}
-diffByAspect(data, types)
+diffReverseAspect(data, types)
